187ec6ced4fb06ab0eb609d69cdb4eb4d7a11017
Jakob Willforss <jakob.willforss@hotmail.com>
2017-12-02 20:13:42 +0100

    Split interaction code to separate file

---
+++ src/interaction.js
@@ -0,0 +1,105 @@
+function onWindowResize( event ) {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+    mouse.x = - event.clientX;
+    mouse.y = event.clientY;
+
+    target.x = targetOnDown.x + ( mouse.x - mouseOnDown.x ) * 0.005;
+    target.y = targetOnDown.y + ( mouse.y - mouseOnDown.y ) * 0.005;
+
+    target.y = target.y > PI_HALF ? PI_HALF : target.y;
+    target.y = target.y < - PI_HALF ? - PI_HALF : target.y;
+
+}
+
+
+
+function onDocumentMouseUp( event ) {
+
+    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+
+    container.style.cursor = 'auto';
+
+}
+
+function onDocumentMouseOut( event ) {
+
+    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+
+}
+
+function onDocumentMouseDown( event ) {
+
+    event.preventDefault();
+
+    mouseVector.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
+    mouseVector.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+
+    raycaster.setFromCamera( mouseVector, camera );
+
+    var intersects = raycaster.intersectObjects( scene.children );
+
+    if ( intersects.length > 0 ) {
+
+
+        var latlng = getEventCenter.call(intersects[0], event);
+        console.log(latlng)
+    }
+
+
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+
+    mouseOnDown.x = - event.clientX;
+    mouseOnDown.y = event.clientY;
+
+    targetOnDown.x = target.x;
+    targetOnDown.y = target.y;
+
+    container.style.cursor = 'move';
+
+}
+
+function getPoint(event) {
+
+    // Get the vertices
+    console.log(this)
+    let a = this.geometry.vertices[event.face.a];
+    let b = this.geometry.vertices[event.face.b];
+    let c = this.geometry.vertices[event.face.c];
+
+    // Averge them together
+    let point = {
+        x: (a.x + b.x + c.x) / 3,
+        y: (a.y + b.y + c.y) / 3,
+        z: (a.z + b.z + c.z) / 3
+    };
+
+    return point;
+}
+
+function getEventCenter(event, radius) {
+    radius = radius || 200;
+
+    var point = this.point;
+
+    var latRads = Math.acos(point.y / radius);
+    var lngRads = Math.atan2(point.z, point.x);
+    var lat = (Math.PI / 2 - latRads) * (180 / Math.PI);
+    var lng = (Math.PI - lngRads) * (180 / Math.PI);
+
+    return [lat, lng - 180];
+}
