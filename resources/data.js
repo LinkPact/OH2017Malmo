3a535e7604657a96b6b2ef1dd4149a7d95a5f76b
danielhamngren <daniel@memoto.com>
2017-12-02 21:19:39 +0100

    Adding project data

---
+++ resources/data.js
@@ -0,0 +1,57 @@
+data = [
+    {
+        "title":"MOROCCO: Strengthening the capacities of the National Human Rights Council (Twinning)",
+        "organization": "Bim",
+        "country": "Morocco",
+        "latLong": {"lat":34.033333, "long":-6.85},
+        "startDate": "2015-07-01",
+        "endDate": "2017-11-01",
+        "focus": [
+            "conflict prevention",
+            "electoral observation",
+            "investigation of human rights violations",
+            "establishment of an NPM",
+            "rights of migrants and asylum seekers" 
+        ],
+        "url": "http://bim.lbg.ac.at/en/publikation/morocco-strengthening-capacities-national-human-rights-council-twinning"
+    },
+    {
+        "title":"KYRGYZSTAN: In-country research on measures and mechanisms which contribute to preventing torture and other ill-treatment in Kyrgyzstan",
+        "organization": "Bim",
+        "country": "Kyrgyzstan",
+        "latLong": {"lat":42.866667, "long":74.6},
+        "startDate": "2014-02-01",
+        "endDate": "2014-12-01",
+        "focus": [
+            "torture prevention methods"
+        ],
+        "url": "http://bim.lbg.ac.at/en/publikation/kyrgyzstan-country-research-measures-and-mechanisms-which-contribute-preventing-torture-and-other-ill-treatment-kyrgyzstan"
+    },
+    {
+        "title":"polis – Austrian Centre for Citizenship Education in Schools",
+        "organization": "Bim",
+        "country": "Austria",
+        "latLong": {"lat":48.2, "long":16.35},
+        "startDate": "2006-02-01",
+        "endDate": "2020-12-01",
+        "focus": [
+            "education",
+            "training",
+            "politics" 
+        ],
+        "url": "http://bim.lbg.ac.at/en/publikation/polis-austrian-centre-citizenship-education-school"
+    },
+    {
+        "title":"BIH Civil Society - Civil Society as a Factor for Change in Bosnia and Herzegovina",
+        "organization": "Bim",
+        "country": "Bosnia and Herzegovina",
+        "latLong": {"lat":43.866667, "long":18.416667},
+        "startDate": "2018-07-01",
+        "endDate": "2019-07-01",
+        "focus": [
+            "political participation", 
+            "social change" 
+        ],
+        "url": "http://bim.lbg.ac.at/en/publikation/bih-civil-society-civil-society-factor-change-bosnia-and-herzegovina"
+    }
+]
